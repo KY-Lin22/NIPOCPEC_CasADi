9e0771f4fb57217bac739ad35bfa0b35f64062ac
KY-Lin22 <linkangyu1994@gmail.com>
2023-02-23 20:37:19 +0900

    example

--- Example/test_AffineDVI.m
+++ Example/test_AffineDVI.m
@@ -5,42 +5,44 @@ delete Gen_InitialGuess.mat
 %% Problem Formulation
 addpath('E:\GitHub\CasADi\casadi-windows-matlabR2016a-v3.5.5')
 import casadi.*
-% FilippovDI: an example from Stewart's paper:
-% Optimal control of systems with discontinuous differential equations
-
-timeStep = 0.02;
+timeStep = 0.01;
 nStages = 100;
-InitState = -1;
-EndState = 5/3;
-RefState = 0;
-x_Dim = 1;
-u_Dim = 0;
+InitState = [-1/2; -1];
+EndState = [0; 0];
+RefState = [0; 0];
+x_Dim = 2;
+u_Dim = 1;
 p_Dim = 1;
 x = SX.sym('x', x_Dim, 1);
 u = SX.sym('u', u_Dim, 1);
-p = SX.sym('p', p_Dim, 1); % variable y in the Stewart's paper
+p = SX.sym('p', p_Dim, 1);
 
 % cost function
-L_T = (x - EndState)^2;
-L_S = (x - RefState)^2;
+xWeight = [20; 20];
+uWeight = 1;
+
+L_T = 0.5*(x - EndState)'*diag(xWeight)*(x - EndState);
+L_S = 0.5*(x - RefState)'*diag(xWeight)*(x - RefState) + 0.5*u'*diag(uWeight)*u;
 
 % inequality constraint
-x_Max = 10000;
-x_Min = -10000;
+x_Max = [2; 2];
+x_Min = [-2; -2];
+u_Max = 2;
+u_Min = -2;
 G = [x_Max - x;...
-    x - x_Min];
+    x - x_Min;...
+    u_Max - u;...
+    u - u_Min];
 
 % equality constraint
 C = [];
 
 % dynamics
-f1 = 1;% switch function > 0
-f2 = 3; % switch function < 0
-f = f1*(1 - p) + f2*p;
+f = [1, -3; -8, 10] * x + [-3; -1] * p + [4; 8] * u; 
 
 % equilibrium constraint
-K = x;
-lbp = 0;
+K = [1, -3] * x + 5 * p + 3 * u;
+lbp = -1;
 ubp = 1;
 
 % formulate OCPEC
@@ -70,11 +72,11 @@ Gen_InitialGuess = load('Gen_InitialGuess.mat');
 Var_Init = Gen_InitialGuess.Var;
 
 %% solving MPEC
-solver.Option.printLevel = 0;
+solver.Option.printLevel = 2;
 solver.Option.maxIterNum = 500;
-solver.Option.Tolerance.KKT_Error_Total = 1e-4;
-solver.Option.Tolerance.KKT_Error_Feasibility = 1e-6;
-solver.Option.Tolerance.KKT_Error_Stationarity = 1e-6;
+solver.Option.Tolerance.KKT_Error_Total = 1e-2;
+solver.Option.Tolerance.KKT_Error_Feasibility = 1e-4;
+solver.Option.Tolerance.KKT_Error_Stationarity = 1e-4;
 
 solver.Option.HessianApproximation = 'CostFunction'; %  'Exact', 'CostFunction', 'GaussNewton'
 solver.Option.RegularParam.nu_J = 1e-7;
@@ -98,40 +100,26 @@ toc
 % iteration process information
 solver.showResult(Info)
 
-% plot solution
 timeAxis = 0 : timeStep : nStages * timeStep;
+K_value = solver.FunObj.K(solution.x, solution.u, solution.p); 
+K_value = full(K_value);
 
-f_value = solver.FunObj.f(solution.x, solution.u, solution.p);
-f_value = full(f_value);
-
-figure(112)
-subplot(3,2,1)
-plot(timeAxis, [InitState, solution.x], 'r', 'LineWidth',1.2)
-legend('x')
+figure(111)
+subplot(3,1,1)
+plot(timeAxis, [InitState(1), solution.x(1, :)], 'r',...
+     timeAxis, [InitState(2), solution.x(2, :)], 'g', 'LineWidth',1.2)
+legend('x1', 'x2')
 xlabel('time(s)')
 title('system state')
 
-subplot(3,2,2)
-plot(timeAxis(2:end), f_value, 'b', 'LineWidth', 1.2)
-legend('f') 
+subplot(3,1,2)
+plot(timeAxis(2:end), solution.u(1,:), 'LineWidth', 1.2)
 xlabel('time(s)')
-title('state equation')
+title('control')
 
-subplot(3,2,3)
-plot(timeAxis(2:end), solution.p, 'k', 'LineWidth', 1.2)
-legend('y') 
+subplot(3,1,3)
+plot(timeAxis(2:end), solution.p(1, :), 'k',...
+     timeAxis(2:end), K_value(1, :), 'b', 'LineWidth', 1.2)
+legend('p', 'K') 
 xlabel('time(s)')
-title('smoothing function')
-
-subplot(3,2,4)
-plot(timeAxis(2:end), solution.x, 'g', 'LineWidth', 1.2)
-legend('z := x') 
-xlabel('time(s)')
-title('switch function')
-
-subplot(3,2,5)
-plot(timeAxis(2:end), solution.x, 'k',...
-    timeAxis(2:end), solution.p, 'b', 'LineWidth', 1.2)
-legend('z(K)', 'y(p)') 
-xlabel('time(s)')
-title('checking BVI')
+title('equilibrium dynamics')
