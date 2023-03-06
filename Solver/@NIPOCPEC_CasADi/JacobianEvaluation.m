1efc7d31c341a00622388818516958e1b62e1b6d
KY-Lin22 <linkangyu1994@gmail.com>
2023-03-06 17:19:49 +0900

    record Jacobian using DM

--- Solver/@NIPOCPEC_CasADi/JacobianEvaluation.m
+++ Solver/@NIPOCPEC_CasADi/JacobianEvaluation.m
@@ -1,11 +1,11 @@
 function Jac = JacobianEvaluation(self, Var, s, mode, FRP)
 %JacobianEvaluation
 %   return struct Jac with fileds including the following values
-% Lx Lu Lp Lw
-% Gx Gu Gp
-% Cx Cu Cp Cw
-% Fx Fu Fp
-% PHIp PHIw
+% Lx Lu Lp Lw(regular recorded as sparse, FRP record as DM type)
+% Gx Gu Gp(record as DM type)
+% Cx Cu Cp Cw(record as DM type)
+% Fx Fu Fp(record as DM type)
+% PHIp PHIw(record as DM type)
 
 OCPEC = self.OCPEC;
 FunObj = self.FunObj;
@@ -17,12 +17,12 @@ switch mode
     case 'Regular'
         [Lx_T, Lp_T] = FunObj.L_T_grad(Var.x(:, end), Var.p(:, end));
         [Lx_S,Lu_S, Lp_S, Lw_S] = FunObj.L_S_grad(Var.x, Var.u, Var.p, Var.w);
-        Lx = full(Lx_S);
-        Lu = full(Lu_S);
-        Lp = full(Lp_S);
-        Lw = full(Lw_S);
-        Lx(:, 1 + (nStages - 1) * Dim.x : end) = Lx(:, 1 + (nStages - 1) * Dim.x : end) + full(Lx_T);
-        Lp(:, 1 + (nStages - 1) * Dim.p : end) = Lp(:, 1 + (nStages - 1) * Dim.p : end) + full(Lp_T);
+        Lx = sparse(Lx_S);
+        Lu = sparse(Lu_S);
+        Lp = sparse(Lp_S);
+        Lw = sparse(Lw_S);
+        Lx(:, 1 + (nStages - 1) * Dim.x : end) = Lx(:, 1 + (nStages - 1) * Dim.x : end) + sparse(Lx_T);
+        Lp(:, 1 + (nStages - 1) * Dim.p : end) = Lp(:, 1 + (nStages - 1) * Dim.p : end) + sparse(Lp_T);
     case 'FRP'
         [Lx, Lu, Lp, Lw] = FunObj.FRP_L_grad(Var.x, Var.u, Var.p, Var.w, FRP.ZRef, FRP.ZWeight);
 end
@@ -34,11 +34,11 @@ end
 [PHIp, PHIw] = FunObj.PHI_grad(Var.p, Var.w, s);
 
 %
-Jac = struct('Lx', full(Lx), 'Lu', full(Lu), 'Lp', full(Lp), 'Lw', full(Lw),...
-    'Gx', full(Gx), 'Gu', full(Gu), 'Gp', full(Gp),...
-    'Cx', full(Cx), 'Cu', full(Cu), 'Cp', full(Cp), 'Cw', full(Cw),...
-    'Fx', full(Fx), 'Fu', full(Fu), 'Fp', full(Fp),...
-    'PHIp', full(PHIp), 'PHIw', full(PHIw));
+Jac = struct('Lx', Lx, 'Lu', Lu, 'Lp', Lp, 'Lw', Lw,...
+    'Gx', Gx, 'Gu', Gu, 'Gp', Gp,...
+    'Cx', Cx, 'Cu', Cu, 'Cp', Cp, 'Cw', Cw,...
+    'Fx', Fx, 'Fu', Fu, 'Fp', Fp,...
+    'PHIp', PHIp, 'PHIw', PHIw);
 
 end
 
